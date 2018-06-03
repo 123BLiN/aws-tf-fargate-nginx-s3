a6183b857cd864aa7119dba4cdd8f6b21882db53
Roman Rusakov <rrusakov@accesssoftek.com>
2018-06-03 22:24:14 +0300

    aws fargate cluster setup

---
+++ README.md
@@ -0,0 +1,3 @@
+# Fargate with Terraform
+
+Example repository to deploy an ECS cluster hosting an nginx with the access to the private S3 bucket
---
+++ variables.tf
@@ -0,0 +1,38 @@
+variable "aws_region" {
+  description = "The AWS region to create things in."
+  default     = "us-west-2"
+}
+
+variable "aws_account_id" {
+  description = "AWS account ID"
+}
+
+variable "az_count" {
+  description = "Number of AZs to cover in a given AWS region"
+  default     = "2"
+}
+
+variable "app_image" {
+  description = "Docker image to run in the ECS cluster"
+  default     = "rrusakov/nginx-aws_auth-ldap:latest"
+}
+
+variable "app_port" {
+  description = "Port exposed by the docker image to redirect traffic to"
+  default     = 80
+}
+
+variable "app_count" {
+  description = "Number of docker containers to run"
+  default     = 3
+}
+
+variable "fargate_cpu" {
+  description = "Fargate instance CPU units to provision (1 vCPU = 1024 CPU units)"
+  default     = "256"
+}
+
+variable "fargate_memory" {
+  description = "Fargate instance memory to provision (in MiB)"
+  default     = "512"
+}
